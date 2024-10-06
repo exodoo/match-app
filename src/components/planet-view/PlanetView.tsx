a0c612fc49f8f5349d31a4702a016b72d9b9098a
Vlad Balabash <vladb951@gmail.com>
2024-10-06 13:11:14 +0200

    planet view code update

--- src/components/planet-view/PlanetView.tsx
+++ src/components/planet-view/PlanetView.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './PlanetView.css';
 
 type PlanetViewProps = {
     planetTexture: string;
     background?: string;
-    name?: string;
 };
 
-export const PlanetView: React.FC<PlanetViewProps> = ({ planetTexture, background, name }) => {
-    const apiUrl = import.meta.env.VITE_PLANET_MICROFE_URL as string;
-    const params = new URLSearchParams();
-    params.append('texture', planetTexture);
-    params.append('background', background || '');
-    const iframeUrl = `${apiUrl}?${params.toString()}`;
+const apiUrl = import.meta.env.VITE_PLANET_MICROFE_URL as string;
+
+export const PlanetView: React.FC<PlanetViewProps> = ({ planetTexture, background }) => {
+    const [planetUrl, setPlanetUrl] = useState<string>('');
+
+    useEffect(() => {
+        const params = new URLSearchParams();
+        params.append('texture', planetTexture);
+        params.append('background', background || '');
+        const iframeUrl = `${apiUrl}?${params.toString()}`;
+        setPlanetUrl(iframeUrl);
+    }, [planetTexture, background]);
 
     return (
         <div>
-            <iframe 
-                src={iframeUrl} 
+            <iframe
+                src={planetUrl}
                 className="planet-view"
                 title="Planet View">
             </iframe>
