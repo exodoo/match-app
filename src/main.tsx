d3dc44822fc3b53907c5aefa095583b648b3c201
Vlad Balabash <vladb951@gmail.com>
2024-10-06 18:28:11 +0200

    fonts fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material';
 import App from './App.tsx';
 import './main.css';
 
+const theme = createTheme({
+  typography: {
+    "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
