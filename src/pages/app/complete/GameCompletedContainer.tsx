068a844dc888a40c4b56370c74f3a4e2e25eea03
NightStrang6r <47156355+NightStrang6r@users.noreply.github.com>
2024-10-06 16:25:50 +0200

    Game completed page

--- src/pages/app/complete/GameCompletedContainer.tsx
+++ src/pages/app/complete/GameCompletedContainer.tsx
@@ -3,7 +3,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 import { Auth, Planets } from "../../../api";
 import { Spinner, Header } from "../../../components";
-import { Timer, Gamers } from "./components";
+import { Timer, Gamers, Matches } from "./components";
 
 import "./GameCompleted.css";
 import { Typography } from "@mui/material";
@@ -31,23 +31,30 @@ const GameCompletedContainer: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    const planetsService = Planets.getInstance();
-    planetsService
-      .getPlanets()
-      .then((planets) => {
-        setResults(planets);
-        if (planets.length > 0) {
-          nav("/app", { replace: true });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+    fetchingTimeoutRef.current = setInterval(() => {
+        const userId = localStorage.getItem('id');
+        Planets.getInstance().getMatches(userId as any)
+            .then((response: any) => {
+                const matches = response?.matches as any;
+                const res = [];
+
+                for (let i = 0; i < matches.length; i++) {
+                    res.push(matches[i].gamer);
+                }
+
+                setResults(res);
+                clearInterval(fetchingTimeoutRef.current!);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        }, 1000);
+    }, []);
 
   return (
     <div className="game-completed">
       <Header />
+      { results.length < 5 ?
       <div className="content-container">
         <div className="greetings-text">
           <h4>Great!</h4>
@@ -58,13 +65,20 @@ const GameCompletedContainer: React.FC = () => {
         </div>
 
         <div className="timer-container">
-          <Timer time={3000} />
+          <Timer duration={300000} />
         </div>
 
         <Spinner />
       </div>
-      Game completed!
-      <Gamers list={[{ name: "Some Name" }]} />
+      : null }
+
+      { results.length >= 5 ?
+      <div className="content-ccontainer">
+        Game completed!
+        <Gamers list={results} />
+        <Matches matchesCount={results.length} />
+      </div>
+        : null }
     </div>
   );
 };
