8badfdb0d2c7f1dddf87b615252012e8bf34e02c
Anatolii <tolka@Anatoliis-MacBook-Pro.local>
2024-10-06 16:12:38 +0200

    small styling

--- src/pages/app/complete/GameCompletedContainer.tsx
+++ src/pages/app/complete/GameCompletedContainer.tsx
@@ -1,83 +1,71 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import { Auth, Planets } from "../../../api";
 import { Spinner, Header } from "../../../components";
 import { Timer, Gamers } from "./components";
-import { Matches } from "./components";
 
 import "./GameCompleted.css";
+import { Typography } from "@mui/material";
 
 const GameCompletedContainer: React.FC = () => {
+  const nav = useNavigate();
   const fetchingTimeoutRef = useRef<number | null>(null);
   const [results, setResults] = useState<any[]>([]);
 
   if (!Auth.getInstance().isLoggedIn()) return <Navigate to="/" replace />;
 
-  const userId = localStorage.getItem('id');
+  useEffect(() => {
+    const planetsService = Planets.getInstance();
+    planetsService
+      .getPlanets()
+      .then((planets) => {
+        setResults(planets);
+        if (planets.length > 0) {
+          nav("/app", { replace: true });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    /*useEffect(() => {
-        const planetsService = Planets.getInstance();
-        planetsService.getPlanets()
-            .then((planets) => {
-                setResults(planets);
-                if (planets.length > 0) {
-                    nav('/app', { replace: true });
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, []);*/
+  useEffect(() => {
+    fetchingTimeoutRef.current = setInterval(() => {
+      // Planets.getInstance().getPlanets()
+      //     .then((planets) => {
+      //         if (planets.length > 0) {
+      //             clearInterval(fetchingTimeoutRef.current!);
+      //         }
+      //     })
+      //     .catch((error) => {
+      //         console.error(error);
+      //     });
+    }, 1000);
+  }, []);
 
-    useEffect(() => {
-        fetchingTimeoutRef.current = setInterval(() => {
-            Planets.getInstance().getMatches(userId as any)
-                .then((response: any) => {
-                    const matches = response?.matches as any;
-                    const res = [];
-
-                    for (let i = 0; i < matches.length; i++) {
-                        res.push(matches[i].gamer);
-                    }
-
-                    setResults(res);
-                    clearInterval(fetchingTimeoutRef.current!);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }, 1000);
-    }, []);
-
-    return (
-        <div className="game-completed">
-          <Header />
-          <div className="content-container">
-            <div className="greetings-text">
-              <h4>Great!</h4>
-              <div className="body-text">
-                You have just chosen 5 exoplanets you like and ready to start. Now
-                you have to wait for match with other 5 astronauts
-              </div>
-            </div>
-    
-            {results.length < 3 ? 
-            <div className="timer-container">
-              <h5>Time left:</h5>
-              <Timer duration={300000} />
-            </div>  
-            : null}
-    
-            <Spinner />
+  return (
+    <div className="game-completed">
+      <Header />
+      <div className="content-container">
+        <div className="greetings-text">
+          <h4>Great!</h4>
+          <div className="body-text">
+            You have just chosen 5 exoplanets you like and ready to start. Now
+            you have to wait for match with other 5 astronauts
           </div>
-          
-          {results.length >= 3 ? <>Game completed!<Gamers list={results}/></>  : null}
-
-          {results.length < 3 ? <>Matches: <Matches matchesCount={results.length} /></> : null}
-          
         </div>
-      );
+
+        <div className="timer-container">
+          <Timer time={3000} />
+        </div>
+
+        <Spinner />
+      </div>
+      Game completed!
+      <Gamers list={[{ name: "Some Name" }]} />
+    </div>
+  );
 };
 
 export default GameCompletedContainer;
